bba13c20db6480ec892d10bf9c0b21743e25c20e
Jorge <jebarria@alumnos.uach.cl>
2021-07-08 01:21:04 -0400

    Implementing the first three main actions from ISR.
    * Action A: change blinking led intensity.
    * Action B: send a "print" command via serial port.
    * Action C: send a "reqleds" command via serial port.

--- DeviceA/DeviceA.ino
+++ DeviceA/DeviceA.ino
@@ -1,41 +1,95 @@
 #include "pinDebouncer.h"
 #include "timeUtils.h"
 #include "ledBlinker.h"
-
-typedef enum {
-  BLINKING_LED,
-  SERIAL_PRINT,
-  SERIAL_LEDS,
-  ESCAPE
-} deviceAction;
+#include "serialLedController.h"
+#include "config.h"
 
 
-static deviceAction currentAction = BLINKING_LED;
-static int pinsToDebounce[] = {4,5,6,7}; // Pins to debounce
-static int prevState;
+typedef struct {
+  int PAState;
+  int PBState;
+  int PCState;
+  int PDState;
+} PushButtonsState;
+
+static int pinsToDebounce[] = {PA, PB, PC, PD}; // Pins to debounce
+static PushButtonsState buttons;
 
 void setup() {
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
+  // Push Buttons are passive, use internal pullups on these inputs
+  pinMode(PA, INPUT_PULLUP);
+  pinMode(PB, INPUT_PULLUP);
+  pinMode(PC, INPUT_PULLUP);
+  pinMode(PD, INPUT_PULLUP);
+  
+  // External interrupt pin is active
+  pinMode(EXT_INT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(EXT_INT_PIN), 
+                  onExternalInterrupt, FALLING);
+
+  // Debouncer setup
   pinDebouncerInit(4, pinsToDebounce);
+  buttons.PAState = pinDebouncerGetPinState(PA);
+  buttons.PBState = pinDebouncerGetPinState(PB);
+  buttons.PCState = pinDebouncerGetPinState(PC);
+  buttons.PDState = pinDebouncerGetPinState(PD);
+  
+  // LEDs Blinker setup
   ledBlinkerInit();
+
+  // Serial port setup
   Serial.begin(115200);
-  prevState = 0;
-  //attachInterrupt(digitalPinToInterrupt(WAKEUP_PIN))
+  serialLedControllerInit();
 }
 
 void loop() {
+  // Update modules periodically
   pinDebouncerUpdate();
   ledBlinkerIterate();
-  int newState = pinDebouncerGetPinState(4);
-  if (newState != prevState) {
-    prevState = newState;
-    if (newState == LOW) {
-      Serial.println("ACTION!");
-      ledBlinkerIntensityCycle();
-    }
+  // Check if there's something in serial port
+  while (Serial.available() > 0) {
+    // Input for the Serial LEDs controller to parse.
+    noInterrupts();
+    serialLedParseChar((char) Serial.read());
+    interrupts();
   }
+}
+
+void onPushButtonA() {
+  ledBlinkerIntensityCycle();
+}
+
+void onPushButtonB() {
+    Serial.print("ELEL329-PRINT:Milliseconds elapsed on device A:");
+    Serial.println(millis());
+}
+
+void onPushButtonC() {
+  Serial.println("ELEL329-REQLEDS");
+}
+
+void onPushButtonD() {
+  // Not implemented yet...
+}
+
+void onExternalInterrupt() {
+  noInterrupts();
+  pinDebouncerUpdate();
+  if ((buttons.PAState == HIGH) && (pinDebouncerGetPinState(PA) == LOW)) {
+    onPushButtonA();
+  }
+  if ((buttons.PBState == HIGH) && (pinDebouncerGetPinState(PB) == LOW)) {
+    onPushButtonB();
+  }
+  if ((buttons.PCState == HIGH) && (pinDebouncerGetPinState(PC) == LOW)) {
+    onPushButtonC();
+  }
+  if ((buttons.PDState == HIGH) && (pinDebouncerGetPinState(PD) == LOW)) {
+    onPushButtonD();
+  }
+  buttons.PAState = pinDebouncerGetPinState(PA);
+  buttons.PBState = pinDebouncerGetPinState(PB);
+  buttons.PCState = pinDebouncerGetPinState(PC);
+  buttons.PDState = pinDebouncerGetPinState(PD);
+  interrupts();
 }
--- DeviceA/config.h
+++ DeviceA/config.h
@@ -3,6 +3,13 @@
 
 #define SLEEP_MODE_PIN 12
 
+/* Pushbuttons */
+#define EXT_INT_PIN 3
+#define PA 4
+#define PB 5
+#define PC 6
+#define PD 7
+
 /* pin debouncer configuration */
 
 #define PIN_DEBOUNCE_TIME_MS 100
@@ -14,13 +21,13 @@
 
 /* led blinker configuration */
 
-#define LED_BLINKER_PIN 13
+#define LED_BLINKER_PIN 13   // L1
 #define LED_BLINKER_PERIOD_MS 500
 
 /* serial led controller configuration */
 
-#define SERIAL_LED_PIN_1 12
-#define SERIAL_LED_PIN_2 11
-#define SERIAL_LED_PIN_3 10
+#define SERIAL_LED_PIN_1 12  // L2
+#define SERIAL_LED_PIN_2 11  // L3
+#define SERIAL_LED_PIN_3 10  // L4
 
 #endif
