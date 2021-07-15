15969a9cb3019491cb2b98e61d5d6bc2e245de21
Jorge <jebarria@alumnos.uach.cl>
2021-07-15 17:49:35 -0400

    Modifying led blinker module implementation.
    Adjusting to "duty cycle" clarification in:
    https://siveducmd.uach.cl/mod/forum/discuss.php?d=56707

--- DeviceA/ledBlinker.c
+++ DeviceA/ledBlinker.c
@@ -5,23 +5,31 @@
 
 /* Constants */
 #define N_DUTY_CYCLES 4
-static const int dutyCycles[] = {64, 128, 192, 255};
+static const float dutyCycles[] = {0.25, 0.50, 0.75, 1.0};
 
 /* Internal variables */
 static unsigned int currentDutyCycleIndex = 0;
 static unsigned long lastBlinkTime = 0;
+static int tOnMilliseconds[4];
+static int tOffMilliseconds[4];
 
+/* Internal functions */
+static void calculateTimes(void);
+static void ledBlinkerIterateOn(void);
+static void ledBlinkerIterateOff(void);
 
 void ledBlinkerInit(void){
 	ledInit(LED_BLINKER_PIN);
-	ledSetIntensity(LED_BLINKER_PIN, dutyCycles[currentDutyCycleIndex]);
+	calculateTimes();
 	lastBlinkTime = 0;
 }
 
 unsigned long ledBlinkerIterate(void){
-	if (millisecondsElapsedSince(lastBlinkTime) >= LED_BLINKER_PERIOD_MS) {
-		lastBlinkTime = getMilliseconds();
-		ledToggle(LED_BLINKER_PIN);
+	if (ledGetState(LED_BLINKER_PIN) == HIGH) {
+		ledBlinkerIterateOn();
+	}
+	else {
+		ledBlinkerIterateOff();
 	}
 	return ledGetState(LED_BLINKER_PIN);
 }
@@ -30,5 +38,29 @@ void ledBlinkerIntensityCycle(void){
 	if (++currentDutyCycleIndex >= N_DUTY_CYCLES) { // do circular iteration
 		currentDutyCycleIndex = 0;
 	}
-	ledSetIntensity(LED_BLINKER_PIN, dutyCycles[currentDutyCycleIndex]);
+}
+
+static void calculateTimes(void) {
+	int i;
+	for (i=0; i<N_DUTY_CYCLES; i++) {
+		tOnMilliseconds[i] = (int) (dutyCycles[i] * LED_BLINKER_PERIOD_MS);
+		tOffMilliseconds[i] = (int) ((1.0 - dutyCycles[i]) * LED_BLINKER_PERIOD_MS);
+	}
+}
+
+static void ledBlinkerIterateOn(void) {
+	if (millisecondsElapsedSince(lastBlinkTime) 
+			>= tOnMilliseconds[currentDutyCycleIndex]) {
+		ledTurnOff(LED_BLINKER_PIN);
+		lastBlinkTime = getMilliseconds();
+
+	}
+}
+
+static void ledBlinkerIterateOff(void) {
+	if (millisecondsElapsedSince(lastBlinkTime) 
+			>= tOffMilliseconds[currentDutyCycleIndex]) {
+		ledTurnOn(LED_BLINKER_PIN);
+		lastBlinkTime = getMilliseconds();
+	}
 }
