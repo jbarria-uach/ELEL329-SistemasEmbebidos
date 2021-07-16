92eeda9c3e62b02b9b94e985e10800d6a5d48e82
Jorge <jebarria@alumnos.uach.cl>
2021-07-16 18:52:08 -0400

    Description comments added for led interface module.

--- DeviceA/ledInterface.c
+++ DeviceA/ledInterface.c
@@ -1,3 +1,20 @@
+/**
+ * @file ledInterface.c
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief LED handling interface source code.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile.
+ *
+ * An interface module to wrap GPIO handling to control LEDs. It supports
+ * PWM intensity control.
+ * @note It may be overkill in the context of this project  because it's used
+ *       just to wrap digitalWrite() calls, but it exists because of initial 
+ *       misunderstanding of "duty cycle" specified in 3.1.
+ * @see https://siveducmd.uach.cl/pluginfile.php/1803649/mod_resource/content/3/Tarea%20ELEL-329%20Programaci%C3%B3n%20de%20Sistemas%20Embebidos.pdf
+ * 
+ */
 #include "Arduino.h"
 #include "ledInterface.h"
 
@@ -14,6 +31,7 @@ static unsigned int nLeds = 0;
 
 static ledState* getLed(int pinNumber);
 
+// Function description in header.
 void ledInit(int pinNumber){
 	if (nLeds < MAX_N_LEDS) {
 		leds[nLeds].state = LOW;
@@ -25,6 +43,7 @@ void ledInit(int pinNumber){
 	}
 }
 
+// Function description in header.
 void ledTurnOn(int pinNumber){
 	ledState* led = getLed(pinNumber);
 	if (led != NULL) {
@@ -38,6 +57,7 @@ void ledTurnOn(int pinNumber){
 	}
 }
 
+// Function description in header.
 void ledTurnOff(int pinNumber){
 	ledState* led = getLed(pinNumber);
 	if (led != NULL) {
@@ -46,6 +66,7 @@ void ledTurnOff(int pinNumber){
 	}
 }
 
+// Function description in header.
 void ledToggle(int pinNumber){
 	ledState* led = getLed(pinNumber);
 	if (led != NULL) {
@@ -58,6 +79,7 @@ void ledToggle(int pinNumber){
 	}
 }
 
+// Function description in header.
 void ledSetIntensity(int pinNumber, int intensity){
 	ledState* led = getLed(pinNumber);
 	if (led != NULL) {
@@ -68,6 +90,7 @@ void ledSetIntensity(int pinNumber, int intensity){
 	}
 }
 
+// Function description in header.
 int ledGetState(int pinNumber){
 	ledState* led = getLed(pinNumber);
 	if (led != NULL) {
@@ -78,6 +101,13 @@ int ledGetState(int pinNumber){
 	}
 }
 
+/**
+ * @brief Get the LED object related with the pin number.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ * @return ledState* a pointer to the corresponding LED ledState struct or 
+ * 					 NULL if there's no LED connected to this.
+ */
 static ledState* getLed(int pinNumber) {
 	int i;
 	for (i=0; i<nLeds; i++) {
--- DeviceA/ledInterface.h
+++ DeviceA/ledInterface.h
@@ -1,3 +1,20 @@
+/**
+ * @file ledInterface.h
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief LED handling interface header.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile.
+ * 
+ * An interface module to wrap GPIO handling to control LEDs. It supports
+ * PWM intensity control.
+ * @note It may be overkill in the context of this project  because it's used
+ *       just to wrap digitalWrite() calls, but it exists because of initial 
+ *       misunderstanding of "duty cycle" specified in 3.1.
+ * @see https://siveducmd.uach.cl/pluginfile.php/1803649/mod_resource/content/3/Tarea%20ELEL-329%20Programaci%C3%B3n%20de%20Sistemas%20Embebidos.pdf
+ * 
+ */
 #ifndef LED_INTERFACE_H
 #define LED_INTERFACE_H
 
@@ -7,11 +24,56 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Initializes the LED pin.
+ * 
+ * This function should be called before handling the LED.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ */
 void ledInit(int pinNumber);
+
+/**
+ * @brief Turns the LED ON.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ */
 void ledTurnOn(int pinNumber);
+
+/**
+ * @brief Turns the LED OFF.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ */
 void ledTurnOff(int pinNumber);
+
+/**
+ * @brief Toggles the LED state.
+ * 
+ * Turns it off if it's on. Turns it on if it's off.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ */
 void ledToggle(int pinNumber);
+
+/**
+ * @brief Sets the LED intensity.
+ * 
+ * It sets the intensity at which the LED should bright whe it's ON.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ * @param intensity the intensity (as PWM duty cycle) at which the LED should
+ *                  bright when it's ON.
+ */
 void ledSetIntensity(int pinNumber, int intensity);
+
+/**
+ * @brief Get the current LED's state.
+ * 
+ * @param pinNumber the number of the pin to which the LED is connected.
+ * @return int the current LED state. (it could be tested with Arduino HIGH
+ *             and LOW Arduino macros).
+ */
 int ledGetState(int pinNumber);
 
 #ifdef __cplusplus
