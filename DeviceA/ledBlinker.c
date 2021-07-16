eac946ea7ffc48a9eb2f0b252a85e53fc3589402
Jorge <jebarria@alumnos.uach.cl>
2021-07-16 16:57:15 -0400

    Documenting the led blinker module

--- DeviceA/ledBlinker.c
+++ DeviceA/ledBlinker.c
@@ -1,3 +1,22 @@
+/**
+ * @file ledBlinker.c
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief LED blinker module source code.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile.
+ * 
+ * A module responsible of controlling a LED blink without using Arduino 
+ * delay() function. It allows duty cycle control cyclyng between four 
+ * modes:
+ *      - Mode 1: 25% duty cycle.
+ *      - Mode 2: 50% duty cycle.
+ *      - Mode 3: 75% duty cycle.
+ *      - Mode 4: 100% duty cycle.
+ * @see https://siveducmd.uach.cl/pluginfile.php/1803649/mod_resource/content/3/Tarea%20ELEL-329%20Programaci%C3%B3n%20de%20Sistemas%20Embebidos.pdf
+ * 
+ */
 #include "Arduino.h"
 #include "timeUtils.h"
 #include "ledBlinker.h"
@@ -18,28 +37,37 @@ static void calculateTimes(void);
 static void ledBlinkerIterateOn(void);
 static void ledBlinkerIterateOff(void);
 
+// Function documentation in header.
 void ledBlinkerInit(void){
 	ledInit(LED_BLINKER_PIN);
 	calculateTimes();
 	lastBlinkTime = 0;
 }
 
-unsigned long ledBlinkerIterate(void){
+// Function documentation in header.
+void ledBlinkerIterate(void){
 	if (ledGetState(LED_BLINKER_PIN) == HIGH) {
 		ledBlinkerIterateOn();
 	}
 	else {
 		ledBlinkerIterateOff();
 	}
-	return ledGetState(LED_BLINKER_PIN);
 }
 
+// Function documentation in header.
 void ledBlinkerIntensityCycle(void){
 	if (++currentDutyCycleIndex >= N_DUTY_CYCLES) { // do circular iteration
 		currentDutyCycleIndex = 0;
 	}
 }
 
+/**
+ * @brief Calculate duty cycle ON/OFF times.
+ * 
+ * This should be called just once at the beginning.
+ * It calculates the duty cycle t_ON and t_OFF in milliseconds given the 
+ * LED_BLINKER_PERIOD_MS definition in config.h.
+ */
 static void calculateTimes(void) {
 	int i;
 	for (i=0; i<N_DUTY_CYCLES; i++) {
@@ -48,6 +76,9 @@ static void calculateTimes(void) {
 	}
 }
 
+/**
+ * @brief Iterate the module activity when the LED is ON.
+ */
 static void ledBlinkerIterateOn(void) {
 	if (millisecondsElapsedSince(lastBlinkTime) 
 			>= tOnMilliseconds[currentDutyCycleIndex]) {
@@ -57,6 +88,9 @@ static void ledBlinkerIterateOn(void) {
 	}
 }
 
+/**
+ * @brief Iterate the module activity when the LED is OFF.
+ */
 static void ledBlinkerIterateOff(void) {
 	if (millisecondsElapsedSince(lastBlinkTime) 
 			>= tOffMilliseconds[currentDutyCycleIndex]) {
--- DeviceA/ledBlinker.h
+++ DeviceA/ledBlinker.h
@@ -1,3 +1,22 @@
+/**
+ * @file ledBlinker.h
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief LED blinker module header.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile.
+ * 
+ * A module responsible of controlling a LED blink without using Arduino 
+ * delay() function. It allows duty cycle control cyclyng between four 
+ * modes:
+ *      - Mode 1: 25% duty cycle.
+ *      - Mode 2: 50% duty cycle.
+ *      - Mode 3: 75% duty cycle.
+ *      - Mode 4: 100% duty cycle.
+ * @see https://siveducmd.uach.cl/pluginfile.php/1803649/mod_resource/content/3/Tarea%20ELEL-329%20Programaci%C3%B3n%20de%20Sistemas%20Embebidos.pdf
+ * 
+ */
 #ifndef LED_BLINKER_H
 #define LED_BLINKER_H
 
@@ -7,8 +26,36 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Initialize LED blinker module.
+ * 
+ * Initializes the module setting internal variables to initial values and the
+ * LED pin mode. 
+ */
 void ledBlinkerInit(void);
-unsigned long ledBlinkerIterate(void);
+
+/**
+ * @brief Iterate module activity.
+ * 
+ * This should be called periodically as fast as possible taking into account
+ * that the calling period T_call = 1/f_call will be equal to the maximum time
+ * error. e.g.:
+ *      - f_call = 10 Hz, e_time <= T_call = 100ms
+ *      - f_call = 100 Hz, e_time <= T_call = 10ms
+ *      - f_call = 500 Hz, e_time <= T_call = 2ms
+ *      - f_call = 1 kHz, e_time <= T_call = 1ms
+ * 
+ * Anyways, given that the human eye perceive 100ms as instantaneous, any 
+ * f_call >= 10 Hz is enough.
+ */
+void ledBlinkerIterate(void);
+
+/**
+ * @brief Changes the LED blinking duty cycle.
+ * 
+ * It cycles between 25%, 50%, 75% and 100% as given by requirements in 3.1
+ * @see https://siveducmd.uach.cl/pluginfile.php/1803649/mod_resource/content/3/Tarea%20ELEL-329%20Programaci%C3%B3n%20de%20Sistemas%20Embebidos.pdf
+ */
 void ledBlinkerIntensityCycle(void);
 
 #ifdef __cplusplus
