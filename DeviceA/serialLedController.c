8b4c3104979e2ce298c13b984db589f738bbceca
Jorge <jebarria@alumnos.uach.cl>
2021-07-16 19:48:50 -0400

    Adding serial LED controller module documentation.

--- DeviceA/serialLedController.c
+++ DeviceA/serialLedController.c
@@ -1,8 +1,20 @@
+/**
+ * @file serialLedController.c
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief Serial operated LED controller source code.
+ * @version 0.1
+ * @date 2021-07-08
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile
+ * 
+ * Parses ELEL329-LEDS commands to change the state of three LEDs.
+ * 
+ */
 #include "serialLedController.h"
 #include "ledInterface.h"
-
 #include <string.h>
 
+/* Constants */
 #define HEADER_STRING "ELEL329-LEDS:"
 #define START_CHAR 'E'
 #define END_CHAR   '\n'
@@ -11,13 +23,17 @@ static const int ledPinNumbers[] = {SERIAL_LED_PIN_1,
 							   	    SERIAL_LED_PIN_2,
 							   	    SERIAL_LED_PIN_3};
 
+/* Internal variables */
 static char serialBuffer[MAX_SERIAL_BUFFER_SIZE];
 static char* bufferPtr;
 static int receivedChars;
 
+/* Internal functions prototypes */
+
 static void processBuffer(void);
 static void resetBuffer(void);
 
+// Description in header file.
 void serialLedControllerInit(void){
 	ledInit(SERIAL_LED_PIN_1);
 	ledInit(SERIAL_LED_PIN_2);
@@ -25,6 +41,7 @@ void serialLedControllerInit(void){
 	resetBuffer();
 }
 
+// Description in header file.
 void serialLedParseChar(char c){
 	if ((receivedChars > 0) || (c == START_CHAR)){
 		*bufferPtr = c;
@@ -39,7 +56,14 @@ void serialLedParseChar(char c){
 	}
 }
 
-
+/**
+ * @brief Process the buffer.
+ * 
+ * It should be called when the end character is detected.
+ * It verifies if the buffer has a valid command and act on the LEDs if so.
+ * It resets the buffer before ending its execution.
+ * 
+ */
 static void processBuffer(void){
 	int i;
 	char* ptr;
@@ -58,6 +82,12 @@ static void processBuffer(void){
 	resetBuffer();
 }
 
+/**
+ * @brief Reset the buffer.
+ * 
+ * It flushes all the data in the buffer.
+ * 
+ */
 static void resetBuffer(void){
 	memset((void *)serialBuffer, 0, 
 		   sizeof(char) * MAX_SERIAL_BUFFER_SIZE);
--- DeviceA/serialLedController.h
+++ DeviceA/serialLedController.h
@@ -1,3 +1,15 @@
+/**
+ * @file serialLedController.h
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief Serial operated LED controller header.
+ * @version 0.1
+ * @date 2021-07-08
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile
+ * 
+ * Parses ELEL329-LEDS commands to change the state of three LEDs.
+ * 
+ */
 #ifndef SERIAL_LED_CONTROLLER_H
 #define SERIAL_LED_CONTROLLER_H
 
@@ -7,7 +19,21 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Module initialization function.
+ * 
+ * Sets the LED pins modes as output and initializes the input buffer.
+ */
 void serialLedControllerInit(void);
+
+/**
+ * @brief Parse a character from the serial port.
+ * 
+ * It should be called for every character that arrives from the serial port.
+ * When a command is succesfully parsed, change the corresponding LEDs state.
+ * 
+ * @param c a character to parse.
+ */
 void serialLedParseChar(char c);
 
 #ifdef __cplusplus
