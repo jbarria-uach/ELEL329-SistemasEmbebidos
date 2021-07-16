656ace0dcf0bccef1712ce370a843f4a6d6232b1
Jorge <jebarria@alumnos.uach.cl>
2021-07-16 19:57:41 -0400

    Adding documentation for "time utils" module.

--- DeviceA/timeUtils.c
+++ DeviceA/timeUtils.c
@@ -1,11 +1,22 @@
+/**
+ * @file timeUtils.c
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief Time related utility functions source code.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile
+ * 
+ */
 #include "Arduino.h"
 #include "timeUtils.h"
 
-
+// Function description in header file.
 unsigned long getMilliseconds(void) {
 	return millis();
 }
 
+// Function description in header file.
 unsigned long millisecondsElapsedSince(unsigned long millisecondsSince){
 	return getMilliseconds() - millisecondsSince;
 }
--- DeviceA/timeUtils.h
+++ DeviceA/timeUtils.h
@@ -1,3 +1,13 @@
+/**
+ * @file timeUtils.h
+ * @author Jorge Barría (jebarria@alumnos.uach.cl)
+ * @brief Time related utility functions header.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021 by Universidad Austral de Chile
+ * 
+ */
 #ifndef TIME_UTILS_H
 #define TIME_UTILS_H
 
@@ -5,7 +15,24 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Get the milliseconds elapsed since the program beginning.
+ * 
+ * It's just a wrapper to the Arduino "millis()" function.
+ * 
+ * @return the number of milliseconds elapsed.
+ */
 unsigned long getMilliseconds(void);
+
+/**
+ * @brief Calculates the number of milliseconds elapsed.
+ * 
+ * @param millisecondsSince the initial value of milliseconds. Must be less
+ *                          than the current milliseconds (as returned by 
+ *                          getMilliseconds() function) elapsed value.
+ * @return the number of milliseconds elapsed since the "millisecondsSince" 
+ *         value.
+ */
 unsigned long millisecondsElapsedSince(unsigned long millisecondsSince);
 
 #ifdef __cplusplus
